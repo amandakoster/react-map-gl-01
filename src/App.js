f381836622ec274ad8841f4c80a8db2050acf43e
Koster, Amanda EX1 <akoster@us.imshealth.com>
2020-09-02 14:51:51 -0700

    adds an SVGOverlay

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import MapGL, { SVGOverlay } from "react-map-gl";
+
+const token = process.env.REACT_APP_TOKEN;
 
 function App() {
+  const [viewport, setViewport] = useState({
+    width: 800,
+    height: 800,
+    longitude: -122.4376,
+    latitude: 37.7577,
+    zoom: 8,
+  });
+
+  const _onViewportChangeLong = (viewport) => {
+    if (viewport.longitude > 0) {
+      viewport.longitude = 0;
+    }
+    setViewport(viewport);
+  };
+
+  const _goToNYC = () => {
+    const newViewport = { ...viewport, longitude: -74.1, latitude: 40.7 };
+    setViewport(newViewport);
+  };
+
+  const _goToSF = () => {
+    const newViewport = {
+      ...viewport,
+      longitude: -122.4376,
+      latitude: 37.7577,
+    };
+    setViewport(newViewport);
+  };
+
+  const redraw = ({ project }) => {
+    const [cx, cy] = project([-122, 37]);
+    return <circle cx={cx} cy={cy} r={4} fill="pink" />;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapGL
+        {...viewport}
+        onViewportChange={_onViewportChangeLong}
+        mapboxApiAccessToken={token}
+      >
+        <SVGOverlay redraw={redraw} />
+      </MapGL>
+      <button onClick={_goToSF}>SF</button>
+      <button onClick={_goToNYC}>NYC</button>
     </div>
   );
 }
