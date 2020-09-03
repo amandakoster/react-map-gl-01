e3e52fbaedf02673274bc716dd4503f193c71fdd
Koster, Amanda EX1 <akoster@us.imshealth.com>
2020-09-02 17:35:38 -0700

    adds navigator.geolocation.getCurrentPosition

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { useState } from "react";
 import "./App.css";
-import MapGL, { SVGOverlay } from "react-map-gl";
-
+import ReactMapGL, { SVGOverlay } from "react-map-gl";
 const token = process.env.REACT_APP_TOKEN;
 
 function App() {
   const [viewport, setViewport] = useState({
-    width: 800,
-    height: 800,
-    longitude: -122.4376,
-    latitude: 37.7577,
-    zoom: 8,
+    width: 500,
+    height: 500,
+    latitude: 47.6062,
+    longitude: -122.3321,
+    zoom: 8
   });
 
-  const _onViewportChangeLong = (viewport) => {
+  const _onViewportChange = (viewport) => {
     if (viewport.longitude > 0) {
       viewport.longitude = 0;
     }
@@ -36,20 +35,35 @@ function App() {
 
   const redraw = ({ project }) => {
     const [cx, cy] = project([-122, 37]);
-    return <circle cx={cx} cy={cy} r={4} fill="pink" />;
+    return <circle cx={cx} cy={cy} r={4} fill="green" />;
   };
+
+  const _setUserLocation = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+      let newLocalViewport = {
+        width: 500,
+        height: 500,
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        zoom: 12
+      }
+      setViewport(newLocalViewport);
+    })
+  }
 
   return (
     <div className="App">
-      <MapGL
+      <ReactMapGL
         {...viewport}
-        onViewportChange={_onViewportChangeLong}
+        mapStyle="mapbox://styles/mapbox/outdoors-v11"
+        onViewportChange={_onViewportChange}
         mapboxApiAccessToken={token}
       >
         <SVGOverlay redraw={redraw} />
-      </MapGL>
+      </ReactMapGL>
       <button onClick={_goToSF}>SF</button>
       <button onClick={_goToNYC}>NYC</button>
+      <button onClick={_setUserLocation }>You are here!</button>
     </div>
   );
 }
