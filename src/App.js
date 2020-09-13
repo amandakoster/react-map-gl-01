b121410365d9483e306698b09a86767df0be46d1
Koster, Amanda EX1 <akoster@us.imshealth.com>
2020-09-13 16:28:24 -0700

    revereted to a class as a null userLocation was breaking with hooks

--- src/App.js
+++ src/App.js
@@ -1,74 +1,96 @@
 import React, { useState } from "react";
 import "./App.css";
-import ReactMapGL, { SVGOverlay } from "react-map-gl";
+import ReactMapGL, { SVGOverlay, Marker } from "react-map-gl";
 const token = process.env.REACT_APP_TOKEN;
 
-function App() {
-  const [state, setState] = useState({
+class App extends React.Component {
+  state = {
     viewport: {
-      width: 500,
-      height: 500,
-      latitude: 47.6062,
-      longitude: -122.3321,
-      zoom: 8,
+      width: "100vw",
+      height: "100vh",
+      latitude: 42.430472,
+      longitude: -123.334102,
+      zoom: 16,
     },
-  });
+    userLocation: {},
+  };
 
-  const _onViewportChange = (viewport) => {
+  onViewportChange = (viewport) => {
     if (viewport.longitude > 0) {
       viewport.longitude = 0;
     }
-    setState({ viewport });
+    this.setState({ viewport });
   };
 
-  const _goToNYC = () => {
-    const newViewport = { ...state.viewport, longitude: -74.1, latitude: 40.7 };
-    setState({ viewport: newViewport });
-  };
-
-  const _goToSF = () => {
+  goToNYC = () => {
     const newViewport = {
-      ...state.viewport,
+      ...this.state.viewport,
+      longitude: -74.1,
+      latitude: 40.7,
+    };
+    this.setState({ viewport: newViewport });
+  };
+
+  goToSF = () => {
+    const newViewport = {
+      ...this.state.viewport,
       longitude: -122.4376,
       latitude: 37.7577,
     };
-    setState({ viewport: newViewport });
+    this.setState({ viewport: newViewport });
   };
 
-  const redraw = ({ project }) => {
-    const [cx, cy] = project([-122, 37]);
-    return <circle cx={cx} cy={cy} r={4} fill="green" />;
-  };
+  // const redraw = ({ project }) => {
+  //   const [cx, cy] = project([-122, 37]);
+  //   return <circle cx={cx} cy={cy} r={4} fill="green" />;
+  // };
 
-  const _setUserLocation = () => {
+  setUserLocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
-      let newLocalViewport = {
-        width: 500,
-        height: 500,
+      let setUserLocation = {
+        lat: position.coords.latitude,
+        long: position.coords.longitude,
+      };
+      let newViewport = {
+        height: "100vh",
+        width: "100vw",
         latitude: position.coords.latitude,
         longitude: position.coords.longitude,
-        zoom: 12,
+        zoom: 10,
       };
-      setState({ viewport: newLocalViewport });
+      this.setState({
+        viewport: newViewport,
+        userLocation: setUserLocation,
+      });
     });
   };
-  console.log("viewport", state);
 
-  return (
-    <div className="App">
-      <ReactMapGL
-        {...state.viewport}
-        mapStyle="mapbox://styles/mapbox/outdoors-v11"
-        onViewportChange={_onViewportChange}
-        mapboxApiAccessToken={token}
-      >
-        <SVGOverlay redraw={redraw} />
-      </ReactMapGL>
-      <button onClick={_goToSF}>SF</button>
-      <button onClick={_goToNYC}>NYC</button>
-      <button onClick={_setUserLocation}>You are here!</button>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.goToSF}>SF</button>
+        <button onClick={this.goToNYC}>NYC</button>
+        <button onClick={this.setUserLocation}>You are here!</button>
+        <ReactMapGL
+          {...this.state.viewport}
+          mapStyle="mapbox://styles/mapbox/outdoors-v11"
+          onViewportChange={this.onViewportChange}
+          mapboxApiAccessToken={token}
+        >
+          {Object.keys(this.state.userLocation).length !== 0 ? (
+            <Marker
+              latitude={this.state.userLocation.lat}
+              longitude={this.state.userLocation.long}
+            >
+              <div>I'm Here!!!</div>
+            </Marker>
+          ) : (
+            <div>Empty</div>
+          )}
+        </ReactMapGL>
+      </div>
+    );
+  }
 }
 
 export default App;
